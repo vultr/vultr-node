a34d0e985daa76de15925a2976ede0607d37476e
Spencer Kordecki <spencerkordecki@gmail.com>
2019-07-08 10:51:14 -0400

    implements #30

--- src/api/snapshot.js
+++ src/api/snapshot.js
@@ -13,3 +13,15 @@ exports.create = {
     }
   }
 }
+
+exports.list = {
+  url: '/snapshot/list',
+  requestType: 'GET',
+  apiKeyRequired: true,
+  parameters: {
+    SNAPSHOTID: {
+      type: 'string',
+      optional: true
+    }
+  }
+}
