677a3fc6cce5093205d6cf46e81a76749a228103
Spencer Kordecki <spencerkordecki@users.noreply.github.com>
2021-09-10 09:51:50 -0400

    add support in instances, bare metal, and applications for marketplace (#522)
    * add support in instances, bare metal, and applications for marketplace
    
    * change image_id parameter type in bare metal create instance

--- test/api/applications.test.js
+++ test/api/applications.test.js
@@ -14,11 +14,23 @@ const mockResponses = {
         id: 1,
         name: 'LEMP',
         short_name: 'lemp',
-        deploy_name: 'LEMP on CentOS 6 x64'
+        deploy_name: 'LEMP on CentOS 6 x64',
+        type: 'one-click',
+        vendor: 'vultr',
+        image_id: ''
+      },
+      {
+        id: 1028,
+        name: 'OpenLiteSpeed WordPress',
+        short_name: 'openlitespeedwordpress',
+        deploy_name: 'OpenLiteSpeed WordPress on Ubuntu 20.04 x64',
+        type: 'marketplace',
+        vendor: 'LiteSpeed_Technologies',
+        image_id: 'openlitespeed-wordpress'
       }
     ],
     meta: {
-      total: 1,
+      total: 2,
       links: {
         next: '',
         prev: ''
